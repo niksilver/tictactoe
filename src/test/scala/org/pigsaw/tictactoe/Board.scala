7fc479bb485ddca20eb55b58fff43381258bf8a3
Nik Silver <nik@niksilver.com>
2015-08-26 20:21:50 +0100

    Refactored Board code to be more intuitive.

--- src/test/scala/org/pigsaw/tictactoe/Board.scala
+++ src/test/scala/org/pigsaw/tictactoe/Board.scala
@@ -2,8 +2,10 @@ package org.pigsaw.tictactoe
 
 class Board() {
   def apply(row: Int, col: Int): Char = {
-    if (col <= -1 || col >= 3) throw new ArrayIndexOutOfBoundsException(s"Column $col out of bounds")
-    if (row <= -1 || row >= 3) throw new ArrayIndexOutOfBoundsException(s"Row $row out of bounds")
+    if (col < 0 || col > 2)
+      throw new ArrayIndexOutOfBoundsException(s"Column $col out of bounds")
+    if (row < 0 || row > 2)
+      throw new ArrayIndexOutOfBoundsException(s"Row $row out of bounds")
     ' '
   }
 }
