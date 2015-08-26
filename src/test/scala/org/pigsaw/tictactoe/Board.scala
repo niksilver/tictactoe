80fd3680039cf8ba72dbf28663fd8af2c383a90b
Nik Silver <nik@niksilver.com>
2015-08-26 20:40:55 +0100

    Can now take a turn on the board.

--- src/test/scala/org/pigsaw/tictactoe/Board.scala
+++ src/test/scala/org/pigsaw/tictactoe/Board.scala
@@ -1,11 +1,24 @@
 package org.pigsaw.tictactoe
 
 class Board() {
+  self =>
+
   def apply(row: Int, col: Int): Char = {
     if (col < 0 || col > 2)
       throw new ArrayIndexOutOfBoundsException(s"Column $col out of bounds")
     if (row < 0 || row > 2)
       throw new ArrayIndexOutOfBoundsException(s"Row $row out of bounds")
     ' '
+  }
+
+  def takeTurn(row: Int, col: Int, token: Char): Board = {
+    new Board() {
+      override def apply(r: Int, c: Int): Char = {
+        if (r == row && c == col)
+          'Z'
+        else
+          self(r, c)
+      }
+    }
   }
 }
--- src/test/scala/org/pigsaw/tictactoe/BoardTest.scala
+++ src/test/scala/org/pigsaw/tictactoe/BoardTest.scala
@@ -67,4 +67,57 @@ class BoardTest extends FlatSpec with ShouldMatchers {
       b(-10, 0)
     }
   }
+
+  "takeTurn" should "place the token in an empty cell" in {
+    val b0 = new Board()
+    b0(0,0) should equal (' ')
+    val b1 = b0.takeTurn(0, 0, 'Z')
+    b1(0,0) should equal ('Z')
+  }
+
+  it should "not place the token in every cell" in {
+    val b0 = new Board()
+    b0(0,0) should equal (' ')
+    b0(0,1) should equal (' ')
+    b0(1,0) should equal (' ')
+
+    val b1 = b0.takeTurn(0, 0, 'Z')
+    b1(0,0) should equal ('Z')
+    b1(0,1) should equal (' ')
+    b1(1,0) should equal (' ')
+  }
+
+  it should "not place the token in every cell - second test to avoid faking" in {
+    val b0 = new Board()
+    b0(2,0) should equal (' ')
+    b0(0,1) should equal (' ')
+    b0(1,0) should equal (' ')
+
+    val b1 = b0.takeTurn(2, 0, 'Z')
+    b1(2,0) should equal ('Z')
+    b1(0,1) should equal (' ')
+    b1(1,0) should equal (' ')
+  }
+
+  it should "allow build up a board over several turns" in {
+    val b0 = new Board()
+    b0(0,1) should equal (' ')
+    b0(1,0) should equal (' ')
+    b0(2,0) should equal (' ')
+
+    val b1 = b0.takeTurn(0, 1, 'Z')
+    b1(0,1) should equal ('Z')
+    b1(1,0) should equal (' ')
+    b1(2,0) should equal (' ')
+
+    val b2 = b1.takeTurn(1, 0, 'Z')
+    b2(0,1) should equal ('Z')
+    b2(1,0) should equal ('Z')
+    b2(2,0) should equal (' ')
+
+    val b3 = b2.takeTurn(2, 0, 'Z')
+    b3(0,1) should equal ('Z')
+    b3(1,0) should equal ('Z')
+    b3(2,0) should equal ('Z')
+  }
 }
