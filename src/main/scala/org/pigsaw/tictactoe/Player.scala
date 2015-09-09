521fc652f6197c01dd270924fda9f537d68687b8
Nik Silver <nik@niksilver.com>
2015-09-09 22:57:20 +0100

    A player will only play on a non-empty cell.

--- src/main/scala/org/pigsaw/tictactoe/Player.scala
+++ src/main/scala/org/pigsaw/tictactoe/Player.scala
@@ -1,13 +1,22 @@
 package org.pigsaw.tictactoe
 
+import scala.annotation.tailrec
+
 /**
  * Created by Nik on 19 Aug 2015.
  */
 class Player(val token: Char) {
 
-  def turn: (Int, Int) = {
-    val row = (Math.random() * 3).toInt
-    val col = (Math.random() * 3).toInt
-    (row, col)
+  def turn(b: Board): (Int, Int) = {
+    @tailrec
+    def calc: (Int, Int) = {
+      val row = (Math.random() * 3).toInt
+      val col = (Math.random() * 3).toInt
+      if (!b.isFilled(row,col))
+        (row, col)
+      else
+        calc
+    }
+    calc
   }
 }
--- src/test/scala/org/pigsaw/tictactoe/PlayerTest.scala
+++ src/test/scala/org/pigsaw/tictactoe/PlayerTest.scala
@@ -14,8 +14,9 @@ class PlayerTest extends FlatSpec with Matchers {
   }
 
   "turn" should "play randomly on an empty board (meaing 10 moves won't be all the same)" in {
+    val b = new Board
     val p = new Player('X')
-    val moves = (1 to 10) map { _ => p.turn }
+    val moves = (1 to 10) map { _ => p.turn(b) }
     val twoMovesAreDifferent = moves exists { m1 => moves exists { m2 => m1 != m2 }}
     twoMovesAreDifferent should be (true)
   }
@@ -24,48 +25,62 @@ class PlayerTest extends FlatSpec with Matchers {
     val p = new Player('O')
     val b = new Board
     (1 to 20).foreach { _ =>
-      val turn = p.turn
+      val turn = p.turn(b)
       // This should not throw an exception
       b.takeTurn(turn._1, turn._2, p.token)
     }
   }
 
   it should "produce different results when starting from scratch" in {
+    val b = new Board
     val length = 20
     val p1 = new Player('Z')
-    val moves1 = (1 to length) map { _ => p1.turn }
+    val moves1 = (1 to length) map { _ => p1.turn(b) }
     val p2 = new Player('Z')
-    val moves2 = (1 to length) map { _ => p2.turn }
+    val moves2 = (1 to length) map { _ => p2.turn(b) }
 
     moves1 should not equal (moves2)
   }
 
   it should "produce different rows when starting from scratch" in {
+    val b = new Board
     val length = 20
     val p1 = new Player('Z')
-    val rows1 = (1 to length) map { _ => p1.turn } map { _._1 }
+    val rows1 = (1 to length) map { _ => p1.turn(b) } map { _._1 }
     val p2 = new Player('Z')
-    val rows2 = (1 to length) map { _ => p2.turn } map { _._1 }
+    val rows2 = (1 to length) map { _ => p2.turn(b) } map { _._1 }
 
     rows1 should not equal (rows2)
   }
 
   it should "produce different columns when starting from scratch" in {
+    val b = new Board
     val length = 20
     val p1 = new Player('Z')
-    val cols1 = (1 to length) map { _ => p1.turn } map { _._2 }
+    val cols1 = (1 to length) map { _ => p1.turn(b) } map { _._2 }
     val p2 = new Player('Z')
-    val cols2 = (1 to length) map { _ => p2.turn } map { _._2 }
+    val cols2 = (1 to length) map { _ => p2.turn(b) } map { _._2 }
 
     cols1 should not equal (cols2)
   }
 
   it should "start on any cell of an empty board given enough goes" in {
+    val b = new Board
     val p = new Player('X')
     val coords = for { i <- 0 to 2; j <- 0 to 2 } yield (i, j)
     coords.foreach { coord =>
-      val starters = (1 to 100) map { _ => p.turn }
+      val starters = (1 to 100) map { _ => p.turn(b) }
       starters should contain (coord)
+    }
+  }
+
+  it should "never play on a filled cell" in {
+    val p = new Player('O')
+    (1 to 100).foreach { _ =>
+      val row = (Math.random() * 3).toInt
+      val col = (Math.random() * 3).toInt
+      val b = (new Board).takeTurn(row, col, 'X')
+      p.turn(b) should not equal ((row, col))
     }
   }
 }
