1d915e37dd8927954f2c52633806020f4263a331
Nik Silver <nik@niksilver.com>
2015-09-12 22:19:18 +0100

    Slowly removing the faking in Game.

--- src/main/scala/org/pigsaw/tictactoe/Game.scala
+++ src/main/scala/org/pigsaw/tictactoe/Game.scala
@@ -3,8 +3,8 @@ package org.pigsaw.tictactoe
 /**
  * Created by Nik on 11 Sep 2015.
  */
-class Game {
+class Game(p1: Player) {
   def state: Seq[Board] = Seq(new Board)
 
-  def turn: Seq[Option[(Int, Int, Char)]] = Seq(Some(0,0,'X'))
+  def turn: Seq[Option[(Int, Int, Char)]] = Seq(Some(0,0, p1.token))
 }
--- src/test/scala/org/pigsaw/tictactoe/GameTest.scala
+++ src/test/scala/org/pigsaw/tictactoe/GameTest.scala
@@ -8,14 +8,24 @@ import org.scalatest.{Matchers, FlatSpec}
 class GameTest extends FlatSpec with Matchers {
 
   "state" should "start with an empty board" in {
-    val g = new Game
+    val g = new Game(new Player('X'))
     val b = g.state(0)
 
     b.isEmpty should be (true)
   }
 
   "turn" should "have some turn as the first turn" in {
-    val g = new Game
+    val g = new Game(new Player('X'))
     g.turn(0) should not be (empty)
+  }
+
+  it should "have the first player's token in the turn" in {
+    val g = new Game(new Player('X'))
+    g.turn(0).get._3 should be ('X')
+  }
+
+  it should "have the first player's token in the turn (with another token to avoid faking)" in {
+    val g = new Game(new Player('Z'))
+    g.turn(0).get._3 should be ('Z')
   }
 }
