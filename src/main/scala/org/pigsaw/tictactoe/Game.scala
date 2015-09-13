fb458bbafa7d988bbe19f10ff33ec923c134b958
Nik Silver <nik@niksilver.com>
2015-09-13 17:38:02 +0100

    Renamed methods in Game.

--- src/main/scala/org/pigsaw/tictactoe/Game.scala
+++ src/main/scala/org/pigsaw/tictactoe/Game.scala
@@ -8,21 +8,21 @@ class Game(p1: Player, p2: Player) {
   private def otherPlayer(p: Player) =
     if (p1.token == p.token) p2 else p1
 
-  def allTurns(accTurns: Seq[(Int,Int)], accBoard: Seq[Board], nextPlayer: Player): (Seq[(Int,Int)], Seq[Board]) = {
+  private def turnsAndBoards(accTurns: Seq[(Int,Int)], accBoard: Seq[Board], nextPlayer: Player): (Seq[(Int,Int)], Seq[Board]) = {
     val b = accBoard.head
     val turn = nextPlayer.turn(b)
     turn match {
       case None => (accTurns, accBoard)
       case Some(coord) => {
         val b2 = b.takeTurn(coord, nextPlayer.token)
-        allTurns(coord +: accTurns, b2 +: accBoard, otherPlayer(nextPlayer))
+        turnsAndBoards(coord +: accTurns, b2 +: accBoard, otherPlayer(nextPlayer))
       }
     }
   }
 
-  val allTurns: (Seq[(Int,Int)], Seq[Board]) = allTurns(Nil, Seq(new Board), p1)
+  private val turnsAndBoards: (Seq[(Int,Int)], Seq[Board]) = turnsAndBoards(Nil, Seq(new Board), p1)
 
-  val turn = allTurns._1.reverse
+  val turns = turnsAndBoards._1.reverse
 
-  val state = allTurns._2.reverse
+  val boards = turnsAndBoards._2.reverse
 }
--- src/test/scala/org/pigsaw/tictactoe/GameTest.scala
+++ src/test/scala/org/pigsaw/tictactoe/GameTest.scala
@@ -7,59 +7,59 @@ import org.scalatest.{Matchers, FlatSpec}
  */
 class GameTest extends FlatSpec with Matchers {
 
-  "state" should "start with an empty board" in {
+  "boards" should "start with an empty board" in {
     val g = new Game(new Player('X'), new Player('O'))
-    val b = g.state(0)
+    val b = g.boards(0)
 
     b.isEmpty should be (true)
   }
 
-  "turn" should "have some first turn" in {
+  "turns" should "have some first turn" in {
     val g = new Game(new Player('X'), new Player('O'))
-    g.turn.length should be >= (1)
+    g.turns.length should be >= (1)
   }
 
-  "state" should "have the first player's token in the turn" in {
+  "boards" should "have the first player's token in the turn" in {
     val g = new Game(new Player('X'), new Player('O'))
-    val t1 = g.turn(0)
-    g.state(1)(t1) should equal('X')
+    val t1 = g.turns(0)
+    g.boards(1)(t1) should equal('X')
   }
 
   it should "have the first player's token in the turn (with another token to avoid faking)" in {
     val g = new Game(new Player('Z'), new Player('Y'))
-    val t1 = g.turn(0)
-    g.state(1)(t1) should equal('Z')
+    val t1 = g.turns(0)
+    g.boards(1)(t1) should equal('Z')
   }
 
   it should "have alternate player tokens for each turn" in {
     val g = new Game(new Player('A'), new Player('B'))
-    g.state(1)(g.turn(0)) should equal('A')
-    g.state(2)(g.turn(1)) should equal('B')
-    g.state(3)(g.turn(2)) should equal('A')
-    g.state(4)(g.turn(3)) should equal('B')
+    g.boards(1)(g.turns(0)) should equal('A')
+    g.boards(2)(g.turns(1)) should equal('B')
+    g.boards(3)(g.turns(2)) should equal('A')
+    g.boards(4)(g.turns(3)) should equal('B')
   }
 
-  "turn" should "have different co-ordinates each turn" in {
+  "turns" should "have different co-ordinates each turn" in {
     val g = new Game(new Player('A'), new Player('B'))
-    g.turn.distinct.length should equal (g.turn.length)
+    g.turns.distinct.length should equal (g.turns.length)
   }
 
   it should "be nine turns long" in {
     val g = new Game(new Player('A'), new Player('B'))
-    g.turn.length should equal (9)
+    g.turns.length should equal (9)
   }
 
-  "board" should "be 10 boards long (empty board + nine turns)" in {
+  "boards" should "be 10 boards long (empty board + nine turns)" in {
     val g = new Game(new Player('A'), new Player('B'))
-    g.state.length should equal (10)
+    g.boards.length should equal (10)
   }
 
-  "turn" should "not play the same turns every time" in {
+  "turns" should "not play the same turns every time" in {
     val sameTurns = for {
       _ <- 1 to 20
       g1 = new Game(new Player('A'), new Player('B'))
       g2 = new Game(new Player('A'), new Player('B'))
-    } yield (g1.turn == g2.turn)
+    } yield (g1.turns == g2.turns)
     sameTurns should contain (false)
   }
 }
